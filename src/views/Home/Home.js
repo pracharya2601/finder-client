70fbaf45a9d26c65ffae7168dc8f13e984775d95
Prakash Acharya <pracharya2601@gmail.com>
2020-08-08 07:50:19 -0700

    pagination modified

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -5,6 +5,7 @@ import _, { lastIndexOf } from 'lodash';
 
 //materialui
 import withStyles from '@material-ui/core/styles/withStyles';
+import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 
